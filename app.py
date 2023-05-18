4efaa730479af5996b18310e1a7b3950cca31b63
Reidylla Lima <reidyllalima@hotmail.com>
2023-05-18 15:53:27 -0300

    API Piadas - Chuck Norris | Categoria

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ async def retorna_piadas(numero_de_piadas: int):
             piada = dado["value"]
             lista_de_piadas.append(piada)
     return {"message": lista_de_piadas}
+
+@app.get("/piadas/categoria/{categoria_da_piada}")
+async def retorna_piadas(categoria_da_piada: str):
+        response_piadas = requests.get(f"https://api.chucknorris.io/jokes/random?category={categoria_da_piada}")
+        if response_piadas.status_code == 200:
+            dado = response_piadas.json()
+            piada = dado["value"]
+        return {"message": piada} 
+
+
+
+
